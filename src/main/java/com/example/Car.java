b9e878a7ced1aeda957b42fd69ca2658ba285c02
ayane.tanaka <ayane.tanaka@rakus.co.jp>
2019-05-27 12:33:01 +0900

    Carクラスの作成

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Car {
+	private int speed;
+	private String color;
+	
+	//set&get
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
